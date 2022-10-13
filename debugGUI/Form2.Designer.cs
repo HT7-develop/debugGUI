ce24bb898ebfae1f6aa31ca411d3038c4c821ccf
jsimonis <js@appsforce.org>
2022-10-13 21:12:48 +0200

    login is done, you can login with creds stored in users table

--- debugGUI/Form2.Designer.cs
+++ debugGUI/Form2.Designer.cs
@@ -34,8 +34,8 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.usernameBox = new System.Windows.Forms.TextBox();
+            this.passwordBox = new System.Windows.Forms.TextBox();
             this.iconPictureBox2 = new FontAwesome.Sharp.IconPictureBox();
             this.iconPictureBox1 = new FontAwesome.Sharp.IconPictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
@@ -55,15 +55,14 @@
             this.LoginPanel.Controls.Add(this.button1);
             this.LoginPanel.Controls.Add(this.label2);
             this.LoginPanel.Controls.Add(this.label1);
-            this.LoginPanel.Controls.Add(this.textBox2);
-            this.LoginPanel.Controls.Add(this.textBox1);
+            this.LoginPanel.Controls.Add(this.usernameBox);
+            this.LoginPanel.Controls.Add(this.passwordBox);
             this.LoginPanel.Controls.Add(this.iconPictureBox2);
             this.LoginPanel.Controls.Add(this.iconPictureBox1);
             this.LoginPanel.Dock = System.Windows.Forms.DockStyle.Right;
-            this.LoginPanel.Location = new System.Drawing.Point(665, 0);
-            this.LoginPanel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.LoginPanel.Location = new System.Drawing.Point(582, 0);
             this.LoginPanel.Name = "LoginPanel";
-            this.LoginPanel.Size = new System.Drawing.Size(285, 600);
+            this.LoginPanel.Size = new System.Drawing.Size(249, 450);
             this.LoginPanel.TabIndex = 0;
             // 
             // iconPictureBox3
@@ -72,11 +71,10 @@
             this.iconPictureBox3.IconChar = FontAwesome.Sharp.IconChar.X;
             this.iconPictureBox3.IconColor = System.Drawing.Color.White;
             this.iconPictureBox3.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.iconPictureBox3.IconSize = 23;
-            this.iconPictureBox3.Location = new System.Drawing.Point(248, 16);
-            this.iconPictureBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.iconPictureBox3.IconSize = 18;
+            this.iconPictureBox3.Location = new System.Drawing.Point(217, 12);
             this.iconPictureBox3.Name = "iconPictureBox3";
-            this.iconPictureBox3.Size = new System.Drawing.Size(23, 24);
+            this.iconPictureBox3.Size = new System.Drawing.Size(20, 18);
             this.iconPictureBox3.TabIndex = 8;
             this.iconPictureBox3.TabStop = false;
             this.iconPictureBox3.Click += new System.EventHandler(this.CloseApp);
@@ -86,9 +84,9 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(43, 80);
+            this.label3.Location = new System.Drawing.Point(38, 60);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(231, 41);
+            this.label3.Size = new System.Drawing.Size(187, 32);
             this.label3.TabIndex = 7;
             this.label3.Text = "RAYCO Planner";
             // 
@@ -98,10 +96,9 @@
             this.button1.FlatAppearance.BorderSize = 0;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(93, 440);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Location = new System.Drawing.Point(94, 355);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(86, 31);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 6;
             this.button1.Text = "Login";
             this.button1.UseVisualStyleBackColor = false;
@@ -110,57 +107,58 @@
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(86, 308);
+            this.label2.Location = new System.Drawing.Point(75, 243);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(93, 28);
+            this.label2.Size = new System.Drawing.Size(76, 21);
             this.label2.TabIndex = 5;
             this.label2.Text = "Password";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(86, 223);
+            this.label1.Location = new System.Drawing.Point(75, 155);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(99, 28);
+            this.label1.Size = new System.Drawing.Size(81, 21);
             this.label1.TabIndex = 4;
             this.label1.Text = "Username";
             // 
-            // textBox2
+            // usernameBox
             // 
-            this.textBox2.BackColor = System.Drawing.Color.White;
-            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox2.ForeColor = System.Drawing.Color.Black;
-            this.textBox2.Location = new System.Drawing.Point(86, 255);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.textBox2.MaxLength = 255;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.PlaceholderText = "John Doe";
-            this.textBox2.Size = new System.Drawing.Size(171, 25);
-            this.textBox2.TabIndex = 3;
+            this.usernameBox.BackColor = System.Drawing.Color.White;
+            this.usernameBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.usernameBox.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.usernameBox.ForeColor = System.Drawing.Color.Black;
+            this.usernameBox.Location = new System.Drawing.Point(75, 191);
+            this.usernameBox.MaxLength = 255;
+            this.usernameBox.Name = "usernameBox";
+            this.usernameBox.PlaceholderText = "John Doe";
+            this.usernameBox.Size = new System.Drawing.Size(150, 20);
+            this.usernameBox.TabIndex = 3;
             // 
-            // textBox1
+            // passwordBox
             // 
-            this.textBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
-            this.textBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.HistoryList;
-            this.textBox1.BackColor = System.Drawing.Color.White;
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox1.ForeColor = System.Drawing.Color.Black;
-            this.textBox1.Location = new System.Drawing.Point(86, 340);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.textBox1.MaxLength = 255;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.PasswordChar = '*';
-            this.textBox1.PlaceholderText = "Password";
-            this.textBox1.Size = new System.Drawing.Size(171, 25);
-            this.textBox1.TabIndex = 2;
+            this.passwordBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.passwordBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.HistoryList;
+            this.passwordBox.BackColor = System.Drawing.Color.White;
+            this.passwordBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.passwordBox.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.passwordBox.ForeColor = System.Drawing.Color.Black;
+            this.passwordBox.Location = new System.Drawing.Point(75, 278);
+            this.passwordBox.MaxLength = 255;
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.PasswordChar = '*';
+            this.passwordBox.PlaceholderText = "Password";
+            this.passwordBox.Size = new System.Drawing.Size(150, 20);
+            this.passwordBox.TabIndex = 2;
             // 
             // iconPictureBox2
             // 
@@ -169,11 +167,9 @@
             this.iconPictureBox2.IconChar = FontAwesome.Sharp.IconChar.Lock;
             this.iconPictureBox2.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(115)))), ((int)(((byte)(103)))), ((int)(((byte)(240)))));
             this.iconPictureBox2.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.iconPictureBox2.IconSize = 37;
-            this.iconPictureBox2.Location = new System.Drawing.Point(25, 328);
-            this.iconPictureBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.iconPictureBox2.Location = new System.Drawing.Point(22, 266);
             this.iconPictureBox2.Name = "iconPictureBox2";
-            this.iconPictureBox2.Size = new System.Drawing.Size(37, 43);
+            this.iconPictureBox2.Size = new System.Drawing.Size(32, 32);
             this.iconPictureBox2.TabIndex = 1;
             this.iconPictureBox2.TabStop = false;
             // 
@@ -184,11 +180,9 @@
             this.iconPictureBox1.IconChar = FontAwesome.Sharp.IconChar.User;
             this.iconPictureBox1.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(115)))), ((int)(((byte)(103)))), ((int)(((byte)(240)))));
             this.iconPictureBox1.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.iconPictureBox1.IconSize = 37;
-            this.iconPictureBox1.Location = new System.Drawing.Point(25, 243);
-            this.iconPictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.iconPictureBox1.Location = new System.Drawing.Point(22, 179);
             this.iconPictureBox1.Name = "iconPictureBox1";
-            this.iconPictureBox1.Size = new System.Drawing.Size(37, 43);
+            this.iconPictureBox1.Size = new System.Drawing.Size(32, 32);
             this.iconPictureBox1.TabIndex = 0;
             this.iconPictureBox1.TabStop = false;
             // 
@@ -199,24 +193,22 @@
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(670, 600);
+            this.pictureBox1.Size = new System.Drawing.Size(586, 450);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(29)))), ((int)(((byte)(49)))));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(950, 600);
+            this.ClientSize = new System.Drawing.Size(831, 450);
             this.ControlBox = false;
             this.Controls.Add(this.LoginPanel);
             this.Controls.Add(this.pictureBox1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form2";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
@@ -233,8 +225,8 @@
         #endregion
 
         private Panel LoginPanel;
-        private TextBox textBox2;
-        private TextBox textBox1;
+        private TextBox usernameBox;
+        private TextBox passwordBox;
         private FontAwesome.Sharp.IconPictureBox iconPictureBox2;
         private FontAwesome.Sharp.IconPictureBox iconPictureBox1;
         private Label label2;
--- debugGUI/Form2.cs
+++ debugGUI/Form2.cs
@@ -9,6 +9,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Data.SqlClient;
+
 namespace debugGUI
 {
     public partial class Form2 : Form
@@ -17,17 +19,8 @@ namespace debugGUI
         {
             InitializeComponent();
         }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
+        SqlConnection conn = new SqlConnection(@"Data Source=LAPTOP-6K52544T;Initial Catalog=rayco;Integrated Security=True");
+       
         private void CloseApp(object sender, EventArgs e)
         {
             // when click on button X on the sign in page, a messagebox will pop-up to ask for confirmation
@@ -35,16 +28,58 @@ namespace debugGUI
             d = MessageBox.Show("Are you sure you want to exit this application","Exit application", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
             if (d == DialogResult.Yes)
             {
-                this.Close();
+                Application.Exit();
             }
         }
 
         private void LoginUser(object sender, EventArgs e)
         {
             // compare input with DB and call Form1 (dashbord) as a class
-            this.Close();
-            Form Dashboard = new Form1();
-            Dashboard.ShowDialog();
+            String username, password;
+            username = usernameBox.Text;
+            password = passwordBox.Text;
+
+            try
+            {
+                conn.Open();
+                String querry = "SELECT * FROM users WHERE name = '"+usernameBox.Text+"' AND password = '"+passwordBox.Text+"'";
+                SqlDataAdapter sda = new SqlDataAdapter(querry, conn);
+              
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+
+                if(dt.Rows.Count > 0 )
+                {
+                    //username = usernameBox.Text;
+                    //password = passwordBox.Text;
+
+                    Form Dashboard = new Form1();
+                    Dashboard.Show();
+                    this.Hide();
+                }
+                else 
+                {
+                    MessageBox.Show("Signin Error", "Invalid credentials");
+                    usernameBox.Clear();
+                    passwordBox.Clear();
+
+                    usernameBox.Focus();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("ERROR");
+            }
+            finally
+            {
+                conn.Close();
+            }
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
